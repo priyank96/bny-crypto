308a47c77d7d4a1fc0a8a40476341d98bd92e014
priyank96 <priyankbhandia@gmail.com>
2022-10-04 18:47:43 -0400

    merge main

--- event_data/api.py
+++ event_data/api.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from price_data import read_price_data
 from risk_measures import Volatility
-from util import plot
+from util import plot_grid
 
 if __name__ == '__main__':
     btc = read_price_data('BTC', '2018-01-01', '2024-01-01')
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     to_plot = pd.DataFrame()
     to_plot['close'] = btc['close']
     to_plot['volatility'] = Volatility.calculate(btc)
-    plot(to_plot)
+    plot_grid(to_plot)
