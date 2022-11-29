07b9d91b65d1c72fa7a98db1c1168f875a01b15e
priyank96 <priyankbhandia@gmail.com>
2022-11-28 23:02:59 -0800

    added get tweets and tweets table

---
+++ event_data/twitter/get_tweets.py
@@ -0,0 +1,44 @@
+import time
+import tweepy
+import sqlite3
+import tqdm
+
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAANhqjwEAAAAAFSduWwMBsRMD%2FDm3M6Fo8OLN%2Bxw%3DFFZnjLddTo9SfoYEQqzIsyhw17CDxgM9w3dSINAYMePWN0hVk1"
+DB_NAME = "./crypto.db"
+
+#  https://docs.tweepy.org/en/stable/auth_tutorial.html#auth-tutorial
+api = tweepy.Client(bearer_token=BEARER_TOKEN,
+                    wait_on_rate_limit=True
+                    )
+conn = sqlite3.connect(DB_NAME)
+
+
+def get_tweets():
+    cursor = conn.cursor()
+    total_user_count = cursor.execute('''SELECT COUNT(*) FROM USER;''').fetchone()[0]
+    cursor.execute('''SELECT ID FROM USER ORDER BY FOLLOWING_COUNT DESC;''')
+    for user_id in tqdm.tqdm(cursor, total=total_user_count):
+        tweets = api.get_users_tweets(
+            id=user_id[0],
+            exclude=['retweets,replies'],
+            tweet_fields=['author_id', 'created_at', 'public_metrics'],
+            max_results=20,
+            start_time='2020-01-01T00:00:00Z'
+        ).data
+        if tweets is None:
+            continue
+        for tweet in tweets:
+            tweet_db = (tweet.id, tweet.text, tweet.author_id, str(tweet.created_at),
+                        tweet.public_metrics['like_count'], tweet.public_metrics['reply_count'],
+                        tweet.public_metrics['retweet_count'], tweet.public_metrics['quote_count']
+                        )
+            conn.execute('''
+                INSERT OR REPLACE INTO TWEET(id,body,author_id,created_at,like_count,reply_count,retweet_count,quote_count)
+                VALUES(?,?,?,?,?,?,?,?);
+            ''', tweet_db)
+        conn.commit()
+        time.sleep(1.5)
+
+
+if __name__ == '__main__':
+    get_tweets()
