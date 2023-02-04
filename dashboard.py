30a2d34ba76019a2adfd1c13fbb6386ab0643eda
Ranadeep Singh <ranadees@andrew.cmu.edu>
2023-02-04 15:38:28 -0500

    Updated Dashboard Layout

--- dashboard.py
+++ dashboard.py
@@ -4,5 +4,19 @@ import numpy as np  # np mean, np random
 import pandas as pd  # read csv, df manipulation
 import plotly.express as px  # interactive charts
 import streamlit as st  # 🎈 data web app development
+import streamlit_helpers
+
+# Page config. Other configs are loaded from .streamlit/config.toml
+st.set_page_config(page_title="CRISys - Cryptocurrency Risk Identification System Dashboard", page_icon="images/crisys_logo.png", layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.markdown(streamlit_helpers.hide_streamlit_style, unsafe_allow_html=True) 
+
+with st.sidebar:
+    st.image("images/crisys_logo.png", use_column_width=True)
+    st.title("Dashboard")
+    st.selectbox("Choose Asset:", ["BTC", "ETH"])
+    st.selectbox("Timeperiod:", ["15m", "1h", "6h", "1d"])
+
+
 
 st.title("Real-Time / Live Data Science Dashboard")
+
---
+++ streamlit_helpers.py
@@ -0,0 +1,5 @@
+hide_streamlit_style = """
+            <style>
+            footer {visibility: hidden;}
+            </style>
+            """
