1bcd452129c93c32ea0f6952a90d02b846bba9d6
priyank96 <priyankbhandia@gmail.com>
2023-03-15 01:32:31 -0400

    reuters embeds similarity calculators

--- event_data/reuters/calculate_cosines.py
+++ event_data/reuters/calculate_cosines.py
@@ -2,23 +2,16 @@ import pickle
 
 import numpy as np
 import pandas as pd
-import tqdm
 from sklearn.metrics.pairwise import cosine_similarity
 
 if __name__ == '__main__':
-    df_train = None
-    with open('df_train.pkl', 'rb') as f:
-        df_train = pd.read_pickle(f)
+    df_train = pd.read_pickle('df_train.pkl')
 
     embeds = df_train['embeds'].to_numpy()
     embeds = np.vstack(embeds)
     print(embeds.shape)
 
-    similarities = []
-    for i in tqdm.tqdm(range(len(embeds))):
-        similarities.append(
-            np.argsort(cosine_similarity(embeds[i].reshape((1, -1)), embeds).reshape(-1))[::-1][1:101]
-        )
-    similarities = np.array(similarities)
+    similarities = cosine_similarity(embeds)
+    print(similarities.shape)
     with open('df_train_similarities.pkl', 'wb') as f:
         pickle.dump(similarities, f)
