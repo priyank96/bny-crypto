e03d77de06149117c01ed4f5231925cb812d575f
Ranadeep Singh <ranadeepsingh96@gmail.com>
2023-05-07 19:13:52 -0400

    Update to remove extra data from helpers

--- streamlit_helpers.py
+++ streamlit_helpers.py
@@ -20,17 +20,6 @@ def round_time(dt, mins_delta=30):
 
 # Using RapidAPI to get a response from the chatbot
 # https://rapidapi.com/stefano-pochet-stefano-pochet-default/api/chatgpt-4-bing-ai-chat-api
-url = "https://chatgpt-4-bing-ai-chat-api.p.rapidapi.com/chatgpt-4-bing-ai-chat-api/0.2/send-message/"
-
-payload = {
-	"bing_u_cookie": "1ehrO3MbFrM37PH57fboByHcyaZi_BCt-tqr38idYUw1oDJXw3v99Vf82MWSS8W4AGU_BaFdBRNShBIy7_o8rBKNEIDYQm_uVxogSsvOWjK7fB12lHgQQWIP5PD0sOBkI49Yd7PSGfXBUTd59Uepxs2UUdmeoAIsRMlSGBYYFSTP6XYa8mz2XQEpvQ_rD8RXbo2uNv0hXMEsvGweujGikyA",
-	"question": "Give me 3 examples of how I can use you."
-}
-headers = {
-	"content-type": "application/x-www-form-urlencoded",
-	"X-RapidAPI-Key": "3544cf43d4msh1ed76e82c2c2d91p11cb52jsnc07ced28aeca",
-	"X-RapidAPI-Host": "chatgpt-4-bing-ai-chat-api.p.rapidapi.com"
-}
 
 class Tweet(object):
     def __init__(self, s, embed_str=False):
