29896351eff5b883bc3075a608dcc2098413f93c
Ranadeep Singh <ranadeepsingh96@gmail.com>
2023-05-10 01:07:19 -0400

    Update to scatter plot points

--- plots/plots.py
+++ plots/plots.py
@@ -184,6 +184,7 @@ def line_plot_double_shared_bars(df, column_x=None, column_y1=None, column_y2=No
     fig.update_layout(yaxis2 = dict(range=[0, df[column_y2].max()]), legend=dict(
         x=0,
         y=legend_y), legend_orientation="h")
+    # fig.for_each_trace(lambda t: t.update(name = '<b>' + t.name +'</b>'))
     fig['data'][0]['showlegend'] = True
     return fig
 
@@ -198,6 +199,7 @@ def line_plot_double_shared_stacked_bars(df, column_x=None, column_y1=None, colu
         x = df[column_x]
 
     fig.add_trace(go.Line(x=x, y=df[column_y1], fill=line_fill1, name=line_name1, marker=dict(color=line_color1)), secondary_y=False)
+    # fig.add_trace(go.Line(x=[x_elem for i, x_elem in enumerate(x) if df[column_y1].iloc[i] >= hline_value], y=[y_elem for y_elem in df[column_y1] if y_elem >= hline_value], fill=line_fill1, name=line_name1, marker=dict(color=line_color1)), secondary_y=False)
     if add_hline:
         fig.add_trace(go.Line(x=x, y=[hline_value]*len(x), name=hline_annotation_text, marker=dict(color=hline_color), line_dash="dash"), secondary_y=False)
     for i, (col_name, line_name, line_color)  in enumerate(zip(column_y2, line_name2, line_color2)):
@@ -216,11 +218,16 @@ def line_plot_double_shared_stacked_bars(df, column_x=None, column_y1=None, colu
         layout_margin_top=10
     # fig.update_yaxes(yaxis_title=yaxis_title1, secondary_y=False)
     # fig.update_yaxes(yaxis_title=yaxis_title2, secondary_y=True)
-    fig.update_layout(xaxis_title=xaxis_title, barmode='relative', margin=dict(l=10, r=10, t=layout_margin_top, b=10), height=graph_height)
-    fig.update_layout(yaxis2 = dict(range=[0, df[column_y2].abs().max()]), legend=dict(
-        x=0,
-        # y=1+(0.2*len(column_y2)),))
-        y=legend_y), legend_orientation="h")
+    fig.update_layout(  xaxis_title=xaxis_title, 
+                        barmode='relative', 
+                        margin=dict(l=10, r=10, t=layout_margin_top, b=10), 
+                        height=graph_height,
+                        font=dict(family="Arial Black",size=100),
+                        yaxis2 = dict(range=[0, df[column_y2].abs().max()]), 
+                        legend=dict(x=0,y=legend_y), 
+                        legend_orientation="h",
+                        )
+    # fig.for_each_trace(lambda t: t.update(name = '<b>' + t.name +'</b>'))
     fig['data'][0]['showlegend'] = True
     return fig
 
@@ -257,7 +264,7 @@ def scatter_plot(df, column_x=None, column_y=None, title='', color_primary=None,
     
     fig.add_trace(go.Scatter(x=df[column_x], y=df[column_y], mode='markers', opacity=opacity, marker_color=color_secondary, marker=dict(size=1), hovertemplate=hover_data, hoverlabel=dict(bgcolor=color_secondary), name="All Tweet Embeddings"))
 
-    fig.add_trace(go.Scatter(x=df[column_x].values[-(lookback_hours*2+1):], y=df[column_y].values[-25:], opacity=0.6, mode='markers', marker=dict(size=20, color=color_primary), name=f"Last {lookback_hours} hours Tweet Embeddings"))
+    fig.add_trace(go.Scatter(x=df[column_x].values[-(lookback_hours*2+1):], y=df[column_y].values[-(lookback_hours*2+1):], opacity=0.6, mode='markers', marker=dict(size=20, color=color_primary), name=f"Last {lookback_hours} hours Tweet Embeddings"))
     
     # Add shapes
     fig.add_trace(go.Scatter(x=[3], y=[0.25], opacity=0.2, mode='markers', marker=dict(size=60, color='red'), name="High Price Fall Risk Zone"))
